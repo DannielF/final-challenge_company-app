9b54da3d2dc55f7725f1e66daa995306d7bbbf34
dannielf <danielgranados1992@gmail.com>
2022-06-10 16:32:16 -0500

    [Upd] Test with StepVerifier in progress

--- Backend/src/test/java/co/com/sofka/questions/usecases/AddAnswerUseCaseTest.java
+++ Backend/src/test/java/co/com/sofka/questions/usecases/AddAnswerUseCaseTest.java
@@ -1,7 +1,68 @@
 package co.com.sofka.questions.usecases;
 
-import static org.junit.jupiter.api.Assertions.*;
+import co.com.sofka.questions.model.AnswerDTO;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.AnswerRepository;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Instant;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 
 class AddAnswerUseCaseTest {
 
+    QuestionRepository questionRepository;
+    AnswerRepository answerRepository;
+    AddAnswerUseCase addAnswerUseCase;
+    GetUseCase getUseCase;
+
+    @BeforeEach
+    public void setup() {
+        MapperUtils mapperUtils = new MapperUtils();
+        answerRepository = mock(AnswerRepository.class);
+        questionRepository = mock(QuestionRepository.class);
+        getUseCase = new GetUseCase(mapperUtils, questionRepository, answerRepository);
+        addAnswerUseCase = new AddAnswerUseCase(mapperUtils, getUseCase, answerRepository);
+    }
+
+    @Test
+    @DisplayName("Test for add an answer to a question")
+    public void addAnswerUseCase() {
+
+        var question = new QuestionDTO();
+        question.setId("1");
+        question.setUserId("1");
+        question.setType("tech");
+        question.setCategory("software");
+        question.setQuestion("¿Que es java?");
+        question.setAnswers(List.of(new AnswerDTO("1234", "1", "1", "answer", Instant.now(), Instant.now())));
+
+        AnswerDTO answerDTO = new AnswerDTO("1234", "1", "1", "answer", Instant.now(), Instant.now());
+
+        when(addAnswerUseCase.apply(answerDTO)).thenReturn(Mono.just(question));
+
+
+        StepVerifier.create(addAnswerUseCase.apply(answerDTO))
+                .expectNextMatches(questionDTO -> {
+                    question.setId("1");
+                    question.setUserId("1");
+                    question.setType("tech");
+                    question.setCategory("software");
+                    question.setQuestion("¿Que es java?");
+                    question.setAnswers(List.of(new AnswerDTO("1234", "1", "1", "answer", Instant.now(), Instant.now())));
+                    return true;
+                })
+                .verifyComplete();
+
+        verify(answerRepository).findAll();
+    }
 }
--- Backend/src/test/java/co/com/sofka/questions/usecases/DeleteUseCaseTest.java
+++ Backend/src/test/java/co/com/sofka/questions/usecases/DeleteUseCaseTest.java
@@ -1,7 +1,41 @@
 package co.com.sofka.questions.usecases;
 
-import static org.junit.jupiter.api.Assertions.*;
+import co.com.sofka.questions.reposioties.AnswerRepository;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class DeleteUseCaseTest {
 
+    QuestionRepository questionRepository;
+    AnswerRepository answerRepository;
+    DeleteUseCase deleteUseCase;
+
+    @BeforeEach
+    public void setup() {
+        questionRepository = mock(QuestionRepository.class);
+        answerRepository = mock(AnswerRepository.class);
+        deleteUseCase = new DeleteUseCase(answerRepository,questionRepository);
+    }
+
+    @Test
+    @DisplayName("Delete a question by its id")
+    void deleteQuestionById() {
+
+        when(questionRepository.deleteById("1")).thenReturn(Mono.empty());
+
+        StepVerifier.create(deleteUseCase.apply("1"))
+                        .expectNext()
+                                .verifyComplete();
+
+
+        verify(questionRepository).deleteById("1");
+    }
 }
