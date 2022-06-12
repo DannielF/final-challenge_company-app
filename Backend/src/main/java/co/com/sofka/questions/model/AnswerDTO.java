fd5f1ccaf73c027a8573c22242c987fff66ac257
dannielf <danielgranados1992@gmail.com>
2022-06-11 19:50:06 -0500

    [Test] addAnswerUseCase done

--- Backend/src/main/java/co/com/sofka/questions/model/AnswerDTO.java
+++ Backend/src/main/java/co/com/sofka/questions/model/AnswerDTO.java
@@ -50,6 +50,10 @@ public class AnswerDTO {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getId() {
         return id;
     }
